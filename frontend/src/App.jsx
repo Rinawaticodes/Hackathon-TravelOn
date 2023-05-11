771aa1678c8d703bbc909a8bfd32e84c7d6cd5aa
JulienBenoit7 <julien-benoit@hotmail.fr>
2023-05-11 17:17:57 +0200

    data paris

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import Hotel from "./pages/Hotel";
-
 import "./App.css";
 
 function App() {
+  const [cityData, setCityData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCityData(data);
+      });
+  }, []);
+  console.warn(cityData);
   return (
     <div className="App">
       <Hotel />

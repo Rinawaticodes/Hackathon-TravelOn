57cdc73be5af2d71b8f04640639585939f8ba29c
Florian GERON <geron-f@outlook.fr>
2023-05-11 16:49:09 +0200

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 import Hotel from "./pages/Hotel";
 
 import "./App.css";
@@ -5,6 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hotel />
     </div>
   );
--- frontend/src/pages/Hotel.jsx
+++ frontend/src/pages/Hotel.jsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Bonjour</h1>
-    </div>
-  );
+  return <div>.</div>;
 }

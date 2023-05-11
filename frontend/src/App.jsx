9f350bd26f6a62067188a074266b9328c7bad536
christophe Dubois <dubois51ch@gmail.com>
2023-05-11 17:14:12 +0200

    card interieur

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
+import Card from "./components/Card";
 import Hotel from "./pages/Hotel";
 
 import "./App.css";
 
+const hotelList = [
+  {
+    id: 1,
+    name: "BEST WESTERN AU TROCADERO",
+    adress: "3 avenue Raymond Poincare, 75116 Paris France",
+    note: "4 étoiles",
+    description:
+      "Situé dans une rue arborée du 16e arrondissement, cet hôtel simple se trouve à 1 minute à pied de la station de métro Trocadéro, à 14 minutes de la tour Eiffel et à 2 km de l'Arc de Triomphe.",
+    photo_url:
+      "https://images.bestwestern.com/bwi/brochures/93261/photos/1024/15587616.jpg",
+    link: "https://www.hotelautrocadero.com/",
+    distance: "0.800 km",
+  },
+];
+
 function App() {
+  const hotel = hotelList[0];
+
   return (
     <div className="App">
       <Hotel />
+      <Card hotel={hotel} />
     </div>
   );
 }

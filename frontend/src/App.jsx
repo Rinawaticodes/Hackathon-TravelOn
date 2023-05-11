242d27e7a36d0342674f01c76f2607ee9ff229fb
christophe Dubois <dubois51ch@gmail.com>
2023-05-11 14:01:46 +0200

    base project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import Home from "./pages/Home";
+import Hotel from "./pages/Hotel";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Hotel />
     </div>
   );
 }

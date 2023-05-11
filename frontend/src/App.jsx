0f7991b1a4f0071395baf1a8a27f02b47e7b1160
Rinawaticodes <kyurei.kim@gmail.com>
2023-05-11 15:05:46 +0200

    add fetch APi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://www.data.gouv.fr/fr/datasets/r/5e283905-7464-4165-9766-6a0e6b4ea57c"
+    )
+      .then((response) => response.json())
+      .then((data) => setDestinations(data));
+  }, []);
+
   return (
     <div className="App">
       <Home />
+      <ul>
+        {destinations.map((destination) => (
+          <li key={destination.id}>{destination.name}</li>
+        ))}
+      </ul>
+
       <p>coucou</p>
     </div>
   );

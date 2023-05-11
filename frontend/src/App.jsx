a448cc20049220b8af4b281006cb6b6677b93c32
khaliev <dev.adlan@icloud.com>
2023-05-11 17:29:17 +0200

    roma city added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
 import Hotel from "./pages/Hotel";
-
 import "./App.css";
 
 function App() {
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5002/Rome`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCities(data);
+      });
+  }, []);
+
+  //  console.log(cities);
   return (
     <div className="App">
+      <ul>
+        {cities.map((city) => (
+          <li key={city.id}>{city.name}</li>
+        ))}
+      </ul>
+
       <Hotel />
     </div>
   );

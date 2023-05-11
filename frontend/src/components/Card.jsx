4f79446743fef6929b5fbff27a6bfd1362faa151
christophe Dubois <dubois51ch@gmail.com>
2023-05-11 20:12:42 +0200

    map error

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,30 +1,39 @@
 import PropTypes from "prop-types";
 
-function Card({ hotel }) {
+function Card({ cityDatas }) {
   return (
-    <figure className="card">
-      <div className="cardHeader">
-        <figcaption>{hotel.name}</figcaption>
-        <img src={hotel.photo_url} alt={hotel.name} className="card" />
-      </div>
-      <div className="cardBody">
-        <p>
-          Vous ète à {hotel.distance} de afiche le input
-          <p>{hotel.description}</p>
-        </p>
-        <button type="button">Reservation</button>
-      </div>
-    </figure>
+    <ul className="hotel-list" id="hotel-list">
+      {cityDatas.map((cityData) => (
+        <li key={cityData.id} className="citydata-item">
+          <figure className="card">
+            <div className="cardHeader">
+              <figcaption>{cityData.name}</figcaption>
+              <img
+                src={cityData.photo_url}
+                alt={cityData.name}
+                className="cardImage"
+              />
+            </div>
+            <div className="cardBody">
+              <p>Vous ète à {cityData.distance} de afiche le input</p> <br />
+              <p>{cityData.description}</p> <br />
+              <button type="button">Reservation</button>
+            </div>
+          </figure>
+        </li>
+      ))}
+    </ul>
   );
 }
 
 Card.propTypes = {
-  hotel: PropTypes.shape({
+  cityDatas: PropTypes.shape({
     name: PropTypes.string.isRequired,
     photo_url: PropTypes.string,
     distance: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
+    map: PropTypes.func.isRequired,
   }).isRequired,
 };
 

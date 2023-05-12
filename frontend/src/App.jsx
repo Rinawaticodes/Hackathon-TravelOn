9859582e9fba5401c9d131172a9f0f0263c97e95
Rinawaticodes <kyurei.kim@gmail.com>
2023-05-12 13:08:47 +0200

    edit css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Hotel from "./pages/Hotel";
 import SearchBar from "./components/SearchBar";
 import Navbar from "./components/Navbar";
@@ -9,17 +8,6 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  const [cityDatas, setCityDatas] = useState([]);
-
-  useEffect(() => {
-    fetch(`http://localhost:5002`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        setCityDatas(data);
-      });
-  }, []);
-  console.warn(cityDatas);
-
   return (
     <div className="App">
       <Navbar />
@@ -27,7 +15,7 @@ function App() {
       <Trip />
       <Hotel />
       <ul className="hotel-list" id="hotel-list">
-        <Card cityDatas={cityDatas} />
+        <Card />
       </ul>
       <Footer />
     </div>

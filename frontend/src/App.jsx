9e6f5034878ea8fb5ced34d75b7782bdbf88fba2
christophe Dubois <dubois51ch@gmail.com>
2023-05-12 09:29:41 +0200

    map OK

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Hotel from "./pages/Hotel";
-import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 import "./App.css";
 
@@ -9,7 +9,7 @@ function App() {
   const [cityDatas, setCityDatas] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/`)
+    fetch(`http://localhost:5002`)
       .then((resp) => resp.json())
       .then((data) => {
         setCityDatas(data);
@@ -21,7 +21,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Hotel />
-      <Card cityDatas={cityDatas} />
+      <ul className="hotel-list" id="hotel-list">
+        <Card cityDatas={cityDatas} />
+      </ul>
     </div>
   );
 }
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,29 +1,25 @@
 import PropTypes from "prop-types";
 
 function Card({ cityDatas }) {
-  return (
-    <ul className="hotel-list" id="hotel-list">
-      {cityDatas.map((cityData) => (
-        <li key={cityData.id} className="citydata-item">
-          <figure className="card">
-            <div className="cardHeader">
-              <figcaption>{cityData.name}</figcaption>
-              <img
-                src={cityData.photo_url}
-                alt={cityData.name}
-                className="cardImage"
-              />
-            </div>
-            <div className="cardBody">
-              <p>Vous ète à {cityData.distance} de afiche le input</p> <br />
-              <p>{cityData.description}</p> <br />
-              <button type="button">Reservation</button>
-            </div>
-          </figure>
-        </li>
-      ))}
-    </ul>
-  );
+  return cityDatas.map((cityData) => (
+    <li key={cityData.id} className="citydata-item">
+      <figure className="card">
+        <div className="cardHeader">
+          <figcaption>{cityData.name}</figcaption>
+          <img
+            src={cityData.photo_url}
+            alt={cityData.name}
+            className="cardImage"
+          />
+        </div>
+        <div className="cardBody">
+          <p>Vous ète à {cityData.distance} de afiche le input</p> <br />
+          <p>{cityData.description}</p> <br />
+          <button type="button">Reservation</button>
+        </div>
+      </figure>
+    </li>
+  ));
 }
 
 Card.propTypes = {
